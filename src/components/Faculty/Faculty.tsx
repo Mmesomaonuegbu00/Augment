db2e2ac5e5c259bfc102e8d2b9d9f6dbcbe96f53
mmesoma onuegbu <mmesomaonuegbu9@gmail.com>
2025-04-10 14:48:08 +0000

    changes

--- src/components/Faculty/Faculty.tsx
+++ src/components/Faculty/Faculty.tsx
@@ -5,6 +5,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 
 const Faculty = () => {
@@ -30,57 +31,73 @@ const Faculty = () => {
                         Learn from business Legends
                     </h1></div>
 
-                <Swiper
-                    modules={[Navigation, Pagination]}
-                    spaceBetween={10} // Space between cards
-                    slidesPerView={1} // Shows 3 cards at a time on larger screens
-                    navigation // Enable arrows
-                    pagination={{ clickable: true }} // Enable dots below the slider
-                    breakpoints={{
-                        640: { slidesPerView: 1 }, // 1 card per view on small screens (mobile)
-                        768: { slidesPerView: 2 }, // 2 cards per view on medium screens
-                        1024: { slidesPerView: 3 },
-                        1600: { slidesPerView: 5 } // 3 cards per view on large screens
-                    }}
-                    className="pt-26 "
-                >
-                    {teachers.map((teacher) => (
-                        <SwiperSlide key={teacher.id}>
-                            <div className='relative w-[350px] h-[300px] sm:h-[500px] lg:h-[550px] rounded-2xl mt-16 mb-16'>
-                                <Image
-                                    src={teacher.src}
-                                    alt={teacher.name}
-                                    fill
-                                    priority
-                                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 400px"
-                                    className="rounded-lg object-cover"
-                                />
-                                <div className="absolute inset-0 bg-gradient-to-t from-[#141414] via-transparent/10 to-transparent/10"></div>
-                                <div className="absolute inset-0 flex flex-col justify-end p-4">
-                                    <h2 className='text-white text-xl md:text-3xl font-sans font-medium pb-2'>
-                                        {teacher.name}
-                                    </h2>
-                                    <hr className='text-gray-300' />
-                                    <p className='text-gray-400 text-sm md:text-2xl font-sans'>
-                                        {teacher.title}
-                                    </p>
-                                    <p className='text-white text-xl md:text-3xl font-sans font-medium pb-2'>
-                                        {teacher.company}
-                                    </p>
 
+                <div className="relative px-6 pt-16">
+                    {/* Navigation buttons */}
+                    <div className="absolute left-0 top-[45%] z-10 cursor-pointer bg-black/30 p-3 rounded-full text-white swiper-button-prev-custom">
+                        <FaArrowLeft size={24} />
+                    </div>
+                    <div className="absolute right-0 top-[45%] z-10 cursor-pointer bg-black/30 p-3 rounded-full text-white swiper-button-next-custom">
+                        <FaArrowRight size={24} />
+                    </div>
+
+                    <Swiper
+                        modules={[Navigation, Pagination]}
+                        spaceBetween={10} // Space between cards
+                        slidesPerView={1} // Shows 3 cards at a time on larger screens
+
+                        pagination={{ clickable: true }} // Enable dots below the slider
+                        breakpoints={{
+                            640: { slidesPerView: 1 }, // 1 card per view on small screens (mobile)
+                            768: { slidesPerView: 2 }, // 2 cards per view on medium screens
+                            1024: { slidesPerView: 3 },
+                            1600: { slidesPerView: 5 } // 3 cards per view on large screens
+                        }}
+                        navigation={{
+                            nextEl: '.swiper-button-next-custom',
+                            prevEl: '.swiper-button-prev-custom'
+                        }}
+
+                        className="pt-26 "
+                    >
+                        {teachers.map((teacher) => (
+                            <SwiperSlide key={teacher.id}>
+                                <div className='relative w-[350px] h-[400px] sm:h-[500px] lg:h-[550px] rounded-2xl mt-16 mb-16'>
+                                    <Image
+                                        src={teacher.src}
+                                        alt={teacher.name}
+                                        fill
+                                        priority
+                                        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 400px"
+                                        className="rounded-lg object-cover"
+                                    />
+                                    <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-transparent/5 to-transparent/10"></div>
+                                    <div className="absolute inset-0 flex flex-col justify-end p-4">
+                                        <h2 className='text-white text-2xl md:text-3xl font-sans font-medium pb-2'>
+                                            {teacher.name}
+                                        </h2>
+
+                                        <p className='text-gray-400 text-xl md:text-2xl font-sans'>
+                                            {teacher.title}
+                                        </p>
+                                        <p className='text-white text-xl md:text-3xl font-sans font-medium pb-2'>
+                                            {teacher.company}
+                                        </p>
+
+                                    </div>
                                 </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
 
-              <div className='flex justify-center'>
-              <button className='border-[1px] border-gray-10 text-gray-100 rounded-full py-2 px-16 flex justify-center mt-6 mb-16'>
-                    Meet Our Faculty
-                </button>
+                    <div className='flex justify-center'>
+                        <button className='border-[1px] border-gray-10 text-gray-100 rounded-full py-2 px-16 flex justify-center mt-6 mb-16'>
+                            Meet Our Faculty
+                        </button>
 
-              </div>
+                    </div>
 
+                </div>
             </div>
         </div>
     );
